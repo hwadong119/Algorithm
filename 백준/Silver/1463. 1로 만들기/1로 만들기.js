ba969ca670fbcb5c16991c8e6320b75c8b99b19f
hwadong <54926206+hwadong119@users.noreply.github.com>
2024-02-04 18:00:35 +0900

    [Silver III] Title: 1로 만들기, Time: 192 ms, Memory: 17432 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const X = Number(input[0])
+const dp = new Array(X + 1).fill(0);
+
+for (let i = 2; i < dp.length; i++) {
+    dp[i] = dp[i-1] + 1;
+
+    if (i % 3 === 0) {
+        dp[i] = Math.min(dp[i], dp[i/3] + 1)
+    }
+
+    if (i % 2 === 0) {
+        dp[i] = Math.min(dp[i], dp[i/2] + 1)
+    }
+}
+
+console.log(dp[X]);
