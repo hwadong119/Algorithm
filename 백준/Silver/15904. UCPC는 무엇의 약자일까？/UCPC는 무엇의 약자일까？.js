0610d1d73c2dc7b50ada99074ef8888f5285e6a5
hwadong <54926206+hwadong119@users.noreply.github.com>
2024-02-21 15:30:28 +0900

    [Silver V] Title: UCPC는 무엇의 약자일까?, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.js
+++ 백준/Silver/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.js
@@ -2,5 +2,12 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const str = input[0];
+const ucpc = "UCPC";
 
-console.log(`I ${/[U].{0,}[C].{0,}[P].{0,}[C]/g.test(str) ? 'love' : 'hate'} UCPC`)
+let cnt = 0;
+for (const x of str) {
+  if (ucpc[cnt] === x && cnt < 4) cnt++;
+  if (cnt > 3) break;
+}
+
+console.log(cnt === 4 ? "I love UCPC" : "I hate UCPC");
